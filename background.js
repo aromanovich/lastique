a93196d0887aaf8b69bf88dfa64dbc8e65e390f3
Anton Romanovich <anthony.romanovich@gmail.com>
2012-09-01 12:28:15 +0600

    Clean up

--- background.js
+++ background.js
@@ -1,27 +1,5 @@
-console = chrome.extension.getBackgroundPage().console;
-log = function() { console.log.apply(console, arguments); }
-
-const LAST_FM_API_KEY = 'db72c405c8e43a7f80d32d714cc12907';
-const LAST_FM_API_SECRET = 'b91a14a2f38b943ff83e3ae308ae606c';
-const LAST_FM_BASE_URL = 'http://ws.audioscrobbler.com/2.0/';
-
-
-chrome.extension.onConnect.addListener(function(port) {
-    port.onMessage.addListener(function(message) {
-        if (message.event == 'start_playing') {
-            scrobbler.startedPlaying(message.song);
-        } else if (message.event == 'continue_playing') {
-            scrobbler.continuedPlaying(message.song.id);
-        }
-    });
-});
-
-
-var lastfm = new LastFMClient({
-    apiKey: LAST_FM_API_KEY,
-    apiSecret: LAST_FM_API_SECRET,
-    apiUrl: LAST_FM_BASE_URL
-});
+var console = chrome.extension.getBackgroundPage().console;
+var log = console.log.bind(console);
 
 
 function PostponedFunction(f) {
@@ -45,6 +23,28 @@ function PostponedFunction(f) {
         }
     }
 }
+
+
+const LAST_FM_API_KEY = 'db72c405c8e43a7f80d32d714cc12907';
+const LAST_FM_API_SECRET = 'b91a14a2f38b943ff83e3ae308ae606c';
+const LAST_FM_BASE_URL = 'http://ws.audioscrobbler.com/2.0/';
+
+var lastfm = new LastFMClient({
+    apiKey: LAST_FM_API_KEY,
+    apiSecret: LAST_FM_API_SECRET,
+    apiUrl: LAST_FM_BASE_URL
+});
+
+
+chrome.extension.onConnect.addListener(function(port) {
+    port.onMessage.addListener(function(message) {
+        if (message.event == 'start_playing') {
+            scrobbler.startedPlaying(message.song);
+        } else if (message.event == 'continue_playing') {
+            scrobbler.continuedPlaying(message.song.id);
+        }
+    });
+});
 
 
 var scrobbler = {
@@ -85,10 +85,8 @@ var scrobbler = {
             method: 'track.updateNowPlaying', 
             artist: this._song.artist,
             track: this._song.name,
-            sk: obtainSessionId(true)
-        }, function(response) {
-            log(response, 'updateNowPlaying');
-        });
+            sk: auth.obtainSessionId(true)
+        }, function(response) {});
         storage.setNowPlaying(this._song.artist, this._song.name);
         if (this._postponedClearNowPlaying) {
             this._postponedClearNowPlaying.execute()
@@ -105,40 +103,40 @@ var scrobbler = {
             artist: this._song.artist,
             track: this._song.name,
             timestamp: timestamp,
-            sk: obtainSessionId(true)
-        }, function(response) {
-            log(response, 'scrobble');
-        });
+            sk: auth.obtainSessionId(true)
+        }, function(response) {});
         storage.addToLastScrobbled(this._song.artist, this._song.name, timestamp);
     }
 }
 
 
 var storage = {
-    _getIconUrl: function(track) {
-        var iconUrl = './default_artist_small.png';
-        if (track.album && track.album.image && track.album.image.length > 0) {
-            iconUrl = track.album.image[0]['#text'].replace('serve/64s', 'serve/34s');
-        }
-        return iconUrl;
-    },
-
-    setNowPlaying: function(artist, track) {
-        var that = this;
+    _getTrackInfo: function(artist, track, callback) {
         lastfm.unsignedCall('GET', {
             method: 'track.getInfo',
             artist: artist,
             track: track
         }, function(response) {
             var track = response.track;
+            
+            var iconUrl;
+            if (track.album && track.album.image && track.album.image.length) {
+                iconUrl = track.album.image[0]['#text'].replace('serve/64s', 'serve/34s');
+            }
 
-            localStorage.nowPlaying = JSON.stringify({
+            callback({
                 track: track.name,
                 trackUrl: track.url,
                 artist: track.artist.name,
                 artistUrl: track.artist.url,
-                iconUrl: that._getIconUrl(track)
+                iconUrl: iconUrl
             });
+        });
+    },
+
+    setNowPlaying: function(artist, track) {
+        this._getTrackInfo(artist, track, function(trackData) {
+            localStorage.nowPlaying = JSON.stringify(trackData);
         });
     },
 
@@ -150,86 +148,81 @@ var storage = {
         if (!localStorage.lastScrobbled) {
             localStorage.lastScrobbled = JSON.stringify([]);
         }
-        var that = this;
-        lastfm.unsignedCall('GET', {
-            method: 'track.getInfo',
-            artist: artist,
-            track: track
-        }, function(response) {
-            var track = response.track;
+        this._getTrackInfo(artist, track, function(trackData) {
+            $.extend(trackData, {timestamp: timestamp});
             var table = JSON.parse(localStorage.lastScrobbled);
-            table.push({
-                track: track.name,
-                trackUrl: track.url,
-                artist: track.artist.name,
-                artistUrl: track.artist.url,
-                iconUrl: that._getIconUrl(track),
-                timestamp: timestamp
-            });
+            table.push(trackData);
             localStorage.lastScrobbled = JSON.stringify(table);
         });
     }
 }
 
 
-function authorizeToken() {
-    var url = 'http://www.last.fm/api/auth/' + 
-            '?api_key=' + LAST_FM_API_KEY +
-            '&token=' + localStorage.token;
-    if (!window.authTabId) {
-        chrome.tabs.create({url: url}, function(tab) {
-            window.authTabId = tab.id;
-            chrome.tabs.onRemoved.addListener(function(tabId) {
-                if (window.authTabId == tabId) {
-                    delete window.authTabId;
+var auth = {
+    authorizeToken: function() {
+        var url = 'http://www.last.fm/api/auth/'
+                + '?api_key=' + LAST_FM_API_KEY
+                + '&token=' + localStorage.token;
+        if (!auth.authTabId) {
+            chrome.tabs.create({url: url}, function(tab) {
+                auth.authTabId = tab.id;
+                chrome.tabs.onRemoved.addListener(function(tabId) {
+                    if (auth.authTabId == tabId) {
+                        delete auth.authTabId;
+                    }
+                });
+            });
+        } else {
+            chrome.tabs.update(auth.authTabId, {selected: true});
+        }
+    },
+
+
+    obtainToken: function() {
+        lastfm.synchronousSignedCall('GET', {
+            method: 'auth.getToken'
+        }, function(response) {
+            localStorage.token = response.token;
+        });
+        return localStorage.token;
+    },
+
+
+    obtainSessionId: function(requireAuthorizationIfNeeded) {
+        if (!localStorage.sessionId) {
+            if (!localStorage.token) {
+                auth.obtainToken();
+                if (requireAuthorizationIfNeeded) {
+                    auth.authorizeToken();
+                }
+                return false;
+            }
+            lastfm.synchronousSignedCall('GET', {
+                method: 'auth.getSession',
+                token: localStorage.token
+            }, function(response) {
+                if (response.error) {
+                    if (response.error == 14 || response.error == 15) {
+                        // token has expired or has not been authorized
+                        if (response.error == 15) {
+                            // token has expired
+                            auth.obtainToken();
+                        }
+                        if (requireAuthorizationIfNeeded) {
+                            auth.authorizeToken();
+                        }
+                    }
+                } else {
+                    localStorage.username = response.session.name;
+                    localStorage.sessionId = response.session.key;
                 }
             });
-        });
-    } else {
-        chrome.tabs.update(window.authTabId, {selected: true});
-    }
-}
-
-
-function obtainToken() {
-    lastfm.synchronousSignedCall('GET', {
-        method: 'auth.getToken'
-    }, function(response) {
-        localStorage.token = response.token;
-    });
-    return localStorage.token;
-}
-
-
-function obtainSessionId(requireAuthorizationIfNeeded) {
-    if (!localStorage.sessionId) {
-        if (!localStorage.token) {
-            obtainToken();
-            if (requireAuthorizationIfNeeded) {
-                authorizeToken();
-            }
-            return false;
         }
-        lastfm.synchronousSignedCall('GET', {
-            method: 'auth.getSession',
-            token: localStorage.token
-        }, function(response) {
-            if (response.error) {
-                if (response.error == 14 || response.error == 15) {
-                    // token has expired or has not been authorized
-                    if (response.error == 15) {
-                        // token has expired
-                        obtainToken();
-                    }
-                    if (requireAuthorizationIfNeeded) {
-                        authorizeToken();
-                    }
-                }
-            } else {
-                localStorage.username = response.session.name;
-                localStorage.sessionId = response.session.key;
-            }
-        });
+        return localStorage.sessionId;
     }
-    return localStorage.sessionId;
 }
+
+Zepto(function($) {
+    auth.obtainSessionId(false);
+    storage.clearNowPlaying();
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,6 @@
+var backgroundPage = chrome.extension.getBackgroundPage();
+
+
 function renderPopup() {
     var table = JSON.parse(localStorage.lastScrobbled || '[]');
     var nowPlaying = JSON.parse(localStorage.nowPlaying || 'false');
@@ -12,6 +15,9 @@ function renderPopup() {
         lastScrobbled: table.slice(-10).reverse(),
         nowPlaying: nowPlaying,
         isNothingToShow: !nowPlaying && table.length == 0,
+        icon: function() {
+            return this.iconUrl || './img/no-icon.png';
+        }
     }, {
         song: T.song
     })
@@ -19,17 +25,18 @@ function renderPopup() {
     $(document.body).html(bodyHtml);
 }
 
+
 function renderUnauthorizedPopup() {
-    $(document.body).html('<a href="#" id="authorize">Авторизуй Lastique</a>')
-                    .on('click', function() {
-        backgroundPage.obtainSessionId(true);
+    var a = $('<a href="#" id="authorize">Авторизуй Lastique</a>').on('click', function() {
+        backgroundPage.auth.obtainSessionId(true);
         return false;
     });
+    $(document.body).html(a);
 }
 
+
 Zepto(function($) {
-    backgroundPage = chrome.extension.getBackgroundPage();
-    if (localStorage.sessionId || backgroundPage.obtainSessionId(false)) {
+    if (localStorage.sessionId || backgroundPage.auth.obtainSessionId(false)) {
         $(document.body).addClass('authorized');
         renderPopup();
     } else {
